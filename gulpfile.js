632c09d7a94513548633b15db8df89c4bc0c1f5e
Donovan <donovan.kim@nsuslab.com>
2016-12-23 21:27:28 +0900

    live reload

--- gulpfile.js
+++ gulpfile.js
@@ -16,18 +16,20 @@ gulp.task('watch', ['default'], function() {
     gulp.watch('server/src/*.ts', ['default']);
 });
 
-gulp.task('serve', ['typescript'], function () {
+gulp.task('serve', ['typescript'], function() {
 
-    gulp.watch('./**/*.ts', ['typescript']);
+    gulp.watch('server/src/**/*.ts', ['typescript']);
 
     nodemon({
-        script: 'server/dist/server.js',
-        ext: 'js',
-    }).on('restart', function () {
-        setTimeout(function () {
-            console.log("reload!");
-            livereload.reload();
-        }, 500);
+        // the script to run the app
+        script: './server/dist/server.js',
+        tasks: ['default'],
+        ext: 'ts json', //den vazw ext: ts json .js gt exoume ts isws gia json to xreiastoume alla 9a exei confict me to serverAssets 9a dw.
+        ignore: ['public/', 'build/', 'frontend/', 'gulpfile.js', 'package.json', 'tsconfig.json']
+    }).on('restart', function() {
+        // when the app has restarted, run livereload.
+        gulp.src('./server/dist/server.js')
+            .pipe(livereload())
+            .pipe(notify('Server restarted, reloading page...'));
     });
-
 });
