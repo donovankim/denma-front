6f1ae7f62edfe76b2a18629fe7806d555fbd0d12
Donovan <donovan.kim@nsuslab.com>
2016-12-25 17:10:49 +0900

    dist 폴더 제외

--- server/dist/database.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var MongoDB = require("mongoose");
-exports.MongoDB = MongoDB;
-MongoDB.connect("mongodb://127.0.0.1:27017/test", function () {
-    console.log("connected to mongo.");
-});
--- server/dist/models/welcomeModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var database_1 = require("../database");
-var mongoose_1 = require("mongoose");
-var welcomeSchema = new mongoose_1.Schema({
-    name: String,
-    adult: Boolean
-});
-var Welcome = database_1.MongoDB.model("Welcome", welcomeSchema);
-exports.Welcome = Welcome;
